e4cd7368da46498e310bdd68581ffb4e7f571f26
Jeann Lucas <jeann.lucas@outlook.com>
2023-07-30 21:33:47 -0300

    Feat: Create Searches for Specific Fields

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router 
   .get("/livros", LivroController.listarLivros)
+  .get("/livros/busca", LivroController.listarLivroEditora)
   .get("/livros/:id", LivroController.listarLivroId)
   .post("/livros", LivroController.cadastrarLivro)
   .put("/livros/:id", LivroController.atualizarLivro)
